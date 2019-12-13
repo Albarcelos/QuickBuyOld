8510592d529c09cdf76cc7a399b90535565e5ec5
alexandreluisbarcelos <alexandreluisbarcelos@gmail.com>
2019-12-13 20:47:28 -0300

    Config EF e criacao Contexto

---
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.ObjetoDeValor;
+
+namespace QuickBuy.Repositorio.Contexto
+{
+    public class QuickBuyContexto : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Pedido> Pedido { get; set; }
+        public DbSet<ItemPedido> ItemPedido { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+    }
+}
