76ee2e0affbe95d001243bd7a1904455a3de0d80
alexandreluisbarcelos <alexandreluisbarcelos@gmail.com>
2019-12-16 17:48:43 -0300

    Linkar Repositorio com Web Criar arquivo config.json Alterar Startup para ler arquivo config.json

--- QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -12,5 +12,9 @@ namespace QuickBuy.Repositorio.Contexto
         public DbSet<ItemPedido> ItemPedido { get; set; }
         public DbSet<FormaPagamento> FormaPagamento { get; set; }
 
+        public QuickBuyContexto(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 }
