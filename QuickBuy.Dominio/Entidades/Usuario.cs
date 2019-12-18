c55fce55a940e5964e324714a6298ba023820bb7
alexandreluisbarcelos <alexandreluisbarcelos@gmail.com>
2019-12-18 19:12:51 -0300

    Configurando 1 pra muitos da classe Usuario com a classe Pedido

--- QuickBuy.Dominio/Entidades/Usuario.cs
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -15,7 +15,7 @@ namespace QuickBuy.Dominio.Entidades
         /// <summary>
         /// Usuario pode ter nemhum ou muitos pedidos
         /// </summary>
-        public ICollection<Pedido> Pedidos { get; set; }
+        public virtual ICollection<Pedido> Pedidos { get; set; }
 
         public override void Validate()
         {
